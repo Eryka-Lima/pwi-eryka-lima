4060e962c2a3ce7dfb04e38e31555a5968cac62e
Eryka Lima <erykalima.contato@gmail.com>
2024-08-09 21:43:09 -0300

    Primeira Pagina WEB

---
+++ configuta-git.bat
@@ -0,0 +1,12 @@
+echo OFF
+
+echo --- configurando Usuario GitHub ---
+git config --global user.name "Eryka Lima"
+git config --global user.email "erykalima.contato@gmail.com
+echo --- Mostrando a configuração atual do GitHUB no Computador
+git config --list
+dir
+echo --- Fim ---
+echo --- Finalizando por hoje ---
+
+pause
