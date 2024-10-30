079fcbfd77ce2718152982d79846657f6f0042b5
Eryka Lima <erykalima.contato@gmail.com>
2024-10-29 22:07:56 -0300

    TesteConexão_SQL

---
+++ TesteConexão_BancodeDados.sql
@@ -0,0 +1,10 @@
+use teste;
+create table cliente
+(
+	codigo_cliente int,
+    nome_do_cliente varchar (255),
+    sobrenome varchar (255),
+    cpf int,
+    cep char (8),
+    telefone varchar (9)
+)
