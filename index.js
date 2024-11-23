5996a4ca38541846c71971e01edd01710b17e5b2
Eryka Lima <erykalima.contato@gmail.com>
2024-11-22 22:22:11 -0300

    Exercícios
    Exercícios em JavaScript

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+    let numero = 9;
+
+    if(numero % 2 === 0) {
+       return console.log('A é igual zero então, A é número par.');
+    } else {
+       return console.log("A não é igual zero então, A é número ímpar.");
+    }
+
+
